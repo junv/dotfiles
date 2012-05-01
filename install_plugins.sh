cef7e03a1dd637e0b29b748577f3132b12a69a77
Jun Valdez <github@sploitlabs.com>
2012-05-01 19:02:48 +0800

    shell script to install plugins

---
+++ install_plugins.sh
@@ -0,0 +1,10 @@
+cd ~/.vim
+mkdir autoload 
+wget https://raw.github.com/tpope/vim-pathogen/HEAD/autoload/pathogen.vim -O autoload/pathogen.vim 
+mkdir bundle 
+cd bundle 
+git clone git://github.com/tpope/vim-endwise.git 
+git clone git://github.com/jiangmiao/auto-pairs.git 
+git clone git://github.com/scrooloose/nerdtree.git 
+
+
